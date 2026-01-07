4735bf57fc0b8f3f938864ed21aa3e12bc8dc55b
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-01-07 17:09:16 -0300

    feat(home): responsive navbar

--- src/components/Home/NavBar.tsx
+++ src/components/Home/NavBar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import diagonalArrow from "../../assets/icons/diagonal-arrow.svg";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -8,7 +9,7 @@ function NavBar() {
   };
 
   useEffect(() => {
-    window.addEventListener("resize", () => setIsOpen(false), false);
+    window.addEventListener("resize", () => setIsOpen(false));
   }, []);
 
   return (
@@ -43,10 +44,11 @@ function NavBar() {
         <div
           className={`
             fixed inset-0 z-50
-            h-screen w-screen
-            bg-white/30 backdrop-blur-xs
+            h-screen w-full
+            bg-white/30 backdrop-blur-sm
             transition-transform duration-300
             ${isOpen ? "handle-menu" : "translate-x-full"}
+            md:translate-x-0 md:bg-transparent md:backdrop-blur-none 
           `}
         >
           <span
@@ -55,6 +57,7 @@ function NavBar() {
               h-[300px] w-[300px] rounded-full bg-deep-teal/20
               transition-transform duration-600
               ${isOpen ? "" : "scale-[0.1]"}
+              md:hidden
             `}
           />
           <span
@@ -63,6 +66,7 @@ function NavBar() {
               h-[300px] w-[300px] rounded-full bg-deep-teal/20
               transition-transform duration-800
               ${isOpen ? "" : "scale-[0.1]"}
+              md:hidden
             `}
           />
 
@@ -70,17 +74,35 @@ function NavBar() {
             className="
               flex h-screen flex-col items-center justify-end
               gap-[100px] py-[40px]
+              md:h-[80px] md:flex-row md:justify-between md:py-0 md:px-[40px]
             "
           >
-            <ul className="flex flex-col gap-2 text-4xl font-bold uppercase text-deep-teal">
+            <ul
+              className="
+                flex flex-col gap-2 text-4xl font-dm-sans font-black uppercase text-deep-teal my-auto
+                md:flex-row md:gap-4 md:text-xl md:font-bitter md:font-bold md:normal-case md:text-steel-blue
+                lg:gap-8
+                xl:gap-12
+              "
+            >
               <li>
-                <a className="block p-2" href="#about" onClick={handleMenu}>
+                <a 
+                  className="
+                    block p-2
+                    md:transition md:hover:-translate-y-1 md:duration-300
+                  " 
+                  href="#about" 
+                  onClick={handleMenu}
+                >
                   About
                 </a>
               </li>
               <li>
                 <a
-                  className="block p-2"
+                  className="
+                    block p-2
+                    md:transition md:hover:-translate-y-1 md:duration-300
+                  "
                   href="#experience"
                   onClick={handleMenu}
                 >
@@ -88,18 +110,32 @@ function NavBar() {
                 </a>
               </li>
               <li>
-                <a className="block p-2" href="#education" onClick={handleMenu}>
+                <a 
+                  className="
+                    block p-2
+                    md:transition md:hover:-translate-y-1 md:duration-300
+                  " 
+                  href="#education" 
+                  onClick={handleMenu}
+                >
                   Education
                 </a>
               </li>
               <li>
-                <a className="block p-2" href="#contact" onClick={handleMenu}>
+                <a 
+                  className="
+                    block p-2
+                    md:transition md:hover:-translate-y-1 md:duration-300
+                  " 
+                  href="#contact" 
+                  onClick={handleMenu}
+                >
                   Contact
                 </a>
               </li>
             </ul>
 
-            <address className="flex flex-col gap-6 not-italic">
+            <address className="flex flex-col gap-6 not-italic md:hidden">
               <div>
                 <h3 className="font-dm-sans font-bold text-steel-blue">
                   EMAIL ADDRESS
@@ -109,7 +145,7 @@ function NavBar() {
                 </p>
               </div>
 
-              <ul className="flex gap-3 font-bitter font-bold text-deep-teal">
+              <ul className="flex justify-between font-bitter font-bold text-deep-teal">
                 <li>
                   <a
                     target="_blank"
@@ -139,6 +175,28 @@ function NavBar() {
                 </li>
               </ul>
             </address>
+
+            <a
+              href="/documents/luiz-felipe-resume.pdf"
+              download="luiz-felipe-resume.pdf"
+              aria-label="Download resume"
+              className="
+              max-md:hidden group relative flex items-center gap-2 h-[40px] w-[40px] cursor-pointer bg-steel-blue rounded-full transition-all duration-300 hover:w-[140px]
+              "
+            >
+              <p 
+                className="
+                  absolute right-[40px] text-xl text-off-white font-bold opacity-0 group-hover:opacity-100 transition-opacity
+                "
+                >
+                Resume
+              </p>
+              <img
+                src={diagonalArrow}
+                alt="Download arrow icon"
+                className="absolute right-[8px]"
+              />
+            </a>
           </div>
         </div>
       </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ function Home() {
   return (
     <div>
       <Header/>
-      asdsad
     </div>
   )
 }
